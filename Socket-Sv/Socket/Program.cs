341f25b6daec58ea933dbcd171fa384d74e7f178
Trần Đức Dũng <36451928+T-DucDung@users.noreply.github.com>
2019-09-05 21:47:44 +0700

    update

--- Socket-Sv/Socket/Program.cs
+++ Socket-Sv/Socket/Program.cs
@@ -3,11 +3,12 @@ using System.Net.Sockets;
 using System.Text;
 using System.Net;
 using System.Threading;
+
 namespace SocketSv
 {
     class Program
     {
-        private const int BUFFER_SIZE = 10;
+        private const int BUFFER_SIZE = 100;
         private const int PORT_NUMBER = 9999;
 
         static ASCIIEncoding encoding = new ASCIIEncoding();
@@ -20,7 +21,6 @@ namespace SocketSv
 
                 TcpListener listener = new TcpListener(address, PORT_NUMBER);
 
-                // 1. listen
                 listener.Start();
 
                 Console.WriteLine("Server started on " + listener.LocalEndpoint);
@@ -32,11 +32,8 @@ namespace SocketSv
                 Thread recvThread = new Thread(()=>ReceiverThread(socket));
 
                 recvThread.Start();
-
-                // 3. send
-                socket.Send(encoding.GetBytes("Hello "));
                 recvThread.Join();
-                // 4. close
+
                 socket.Close();
                 listener.Stop();
             }
@@ -51,12 +48,38 @@ namespace SocketSv
         {
             while (true)
             {
-                byte[] data = new byte[BUFFER_SIZE];
-                sock.Receive(data);
+                try
+                {
+                    
+                    byte[] data = new byte[BUFFER_SIZE];
+                    sock.Receive(data);
 
-                string str = encoding.GetString(data);
+                    string str = encoding.GetString(data);
+                    string[] num = str.Split(' ');
+                    int[] arr = new int[num.Length];
 
-                Console.WriteLine(str);
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        arr[i] = Convert.ToInt32(num[i]);
+                    }
+                    Console.Write("Chuoi nhan : ");
+                    foreach (int i in arr)
+                    {
+                        Console.Write($"{i} ");
+                    }
+                    Array.Sort(arr);
+                    String s = "";
+                    Console.Write("\nChuoi tra : ");
+                    foreach (int i in arr)
+                    {
+                        Console.Write($"{i} ");
+                        s = s + Convert.ToString(i) + " ";
+                    }
+                    sock.Send(encoding.GetBytes(s));
+                }catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
             }
         }
     }
